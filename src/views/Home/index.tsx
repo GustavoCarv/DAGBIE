3e9f056fa30c2720edbef3ca45b6d1a325d48a77
GustavoCarv <gustavo.carvg@gmail.com>
2022-09-19 20:01:40 -0300

    feat(home): adicionado logo e redirect na home/ correção vercel

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,20 @@
 import * as Styles from './styles'
+import LOGO from '../../assets/dagbie_full_logo.svg'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const Home: React.FC = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/login')
+    }, 4000)
+  }, [])
+
   return (
     <Styles.Container>
-      <h1>Home</h1>
+      <img src={LOGO} width={508} height={180} />
     </Styles.Container>
   )
 }
